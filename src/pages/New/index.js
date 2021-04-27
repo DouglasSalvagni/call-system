2e4dd62abb30e7758ae305f5e106af5e1655aab6
douglassalvagni <douglassalvagni@gmail.com>
2021-04-27 11:12:40 +0100

    stable

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,30 +1,164 @@
-import { useState } from 'react';
+
+import { useState, useEffect, useContext } from 'react';
+
+import firebase from '../../services/firebaseConnection';
+import { useHistory, useParams } from 'react-router-dom';
+
 import Header from '../../components/Header';
 import Title from '../../components/Title';
+import { AuthContext } from '../../context/auth'; 
+import { toast } from 'react-toastify';
 
 import './new.css';
 import { FiPlusCircle } from 'react-icons/fi'
 
 export default function New(){
+  const { id } = useParams();
+  const history = useHistory();
+
+  const [loadCustomers, setLoadCustomers] = useState(true);
+  const [customers, setCustomers] = useState([]);
+  const [customerSelected, setCustomerSelected] = useState(0);
 
   const [assunto, setAssunto] = useState('Suporte');
   const [status, setStatus] = useState('Aberto');
   const [complemento, setComplemento] = useState('');
 
+  const [idCustomer, setIdCustomer] = useState(false);
 
-  function handleRegister(e){
-    e.preventDefault();
+  const { user } = useContext(AuthContext);
 
-    alert('TESTE')
+
+  useEffect(()=> {
+    async function loadCustomers(){
+      await firebase.firestore().collection('customers')
+      .get()
+      .then((snapshot)=>{
+        let lista = [];
+
+        snapshot.forEach((doc) => {
+          lista.push({
+            id: doc.id,
+            nomeFantasia: doc.data().nomeFantasia
+          })
+        })
+
+        if(lista.length === 0){
+          console.log('NENHUMA EMPRESA ENCONTRADA');
+          setCustomers([ { id: '1', nomeFantasia: 'FREELA' } ]);
+          setLoadCustomers(false);
+          return;
+        }
+
+        setCustomers(lista);
+        setLoadCustomers(false);
+
+        if(id){
+          loadId(lista);
+        }
+
+      })
+      .catch((error)=>{
+        console.log('DEU ALGUM ERRO!', error);
+        setLoadCustomers(false);
+        setCustomers([ { id: '1', nomeFantasia: '' } ]);
+      })
+
+    }
+
+    loadCustomers();
+
+  }, [id]);
+
+
+
+  async function loadId(lista){
+    await firebase.firestore().collection('chamados').doc(id)
+    .get()
+    .then((snapshot) => {
+      setAssunto(snapshot.data().assunto);
+      setStatus(snapshot.data().status);
+      setComplemento(snapshot.data().complemento)
+
+      let index = lista.findIndex(item => item.id === snapshot.data().clienteId );
+      setCustomerSelected(index);
+      setIdCustomer(true);
+
+    })
+    .catch((err)=>{
+      console.log('ERRO NO ID PASSADO: ', err);
+      setIdCustomer(false);
+    })
   }
 
-  function handleChangeSelect(e) {
+  async function handleRegister(e){
+    e.preventDefault();
+
+    if(idCustomer){
+      await firebase.firestore().collection('chamados')
+      .doc(id)
+      .update({
+        cliente: customers[customerSelected].nomeFantasia,
+        clienteId: customers[customerSelected].id,
+        assunto: assunto,
+        status: status,
+        complemento: complemento,
+        userId: user.uid
+      })
+      .then(()=>{
+        toast.success('Chamado Editado com sucesso!');
+        setCustomerSelected(0);
+        setComplemento('');
+        history.push('/dashboard');
+      })
+      .catch((err)=>{
+        toast.error('Ops erro ao registrar, tente mais tarde.')
+        console.log(err);
+      })
+
+      return;
+    }
+
+    await firebase.firestore().collection('chamados')
+    .add({
+      created: new Date(),
+      cliente: customers[customerSelected].nomeFantasia,
+      clienteId: customers[customerSelected].id,
+      assunto: assunto,
+      status: status,
+      complemento: complemento,
+      userId: user.uid
+    })
+    .then(()=> {
+      toast.success('Chamado criado com sucesso!');
+      setComplemento('');
+      setCustomerSelected(0);
+    })
+    .catch((err)=> {
+      toast.error('Ops erro ao registrar, tente mais tarde.')
+      console.log(err);
+    })
+
+
+  }
+
+
+  //Chamado quando troca o assunto
+  function handleChangeSelect(e){
     setAssunto(e.target.value);
   }
 
+
+  //Chamado quando troca o status
   function handleOptionChange(e){
     setStatus(e.target.value);
+  }
 
+  //Chamado quando troca de cliente
+  function handleChangeCustomers(e){
+    //console.log('INDEX DO CLIENTE SELECIONADO: ', e.target.value);
+    //console.log('Cliente selecionado ', customers[e.target.value])
+    setCustomerSelected(e.target.value);
   }
 
   return(
@@ -41,11 +175,20 @@ export default function New(){
           <form className="form-profile"  onSubmit={handleRegister} >
             
             <label>Cliente</label>
-            <select>
-              <option key={1} value={1}>
-                Sujeito Programador
-              </option>
-            </select>
+
+            {loadCustomers ? (
+              <input type="text" disabled={true} value="Carregando clientes..." />
+            ) : (
+                <select value={customerSelected} onChange={handleChangeCustomers} >
+                {customers.map((item, index) => {
+                  return(
+                    <option key={item.id} value={index} >
+                      {item.nomeFantasia}
+                    </option>
+                  )
+                })}
+              </select>
+            )}
 
             <label>Assunto</label>
             <select value={assunto} onChange={handleChangeSelect}>
@@ -61,7 +204,7 @@ export default function New(){
               name="radio"
               value="Aberto"
               onChange={handleOptionChange}
-              checked={ status === 'Aberto'}
+              checked={ status === 'Aberto' }
               />
               <span>Em Aberto</span>
 
@@ -70,7 +213,7 @@ export default function New(){
               name="radio"
               value="Progresso"
               onChange={handleOptionChange}
-              checked={ status === 'Progresso'}
+              checked={ status === 'Progresso' }
               />
               <span>Progresso</span>
 
@@ -79,7 +222,7 @@ export default function New(){
               name="radio"
               value="Atendido"
               onChange={handleOptionChange}
-              checked={ status === 'Atendido'}
+              checked={ status === 'Atendido' }
               />
               <span>Atendido</span>
             </div>
@@ -89,7 +232,7 @@ export default function New(){
               type="text"
               placeholder="Descreva seu problema (opcional)."
               value={complemento}
-              onChange={(e) => setComplemento(e.target.value)}
+              onChange={ (e) => setComplemento(e.target.value) }
             />
             
             <button type="submit">Registrar</button>
